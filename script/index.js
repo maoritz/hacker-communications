c279397d9247c67e21c2715083df14c3d6d5a897
maoritz <maorattias17@gmail.com>
2021-02-27 22:41:07 +0200

    buttons works

--- script/index.js
+++ script/index.js
@@ -1,10 +1,15 @@
 $( document ).ready(function() {
-    $('.menu-button').click(function(){
-        const navList = $('.main-nav')
+    $('.js--menu-button').click(function(){
+        const navList = $('.js--main-nav')
+        const icon = $('.menu-icon')
+
         if(navList.css('display') == 'none'){
             navList.css('display','flex')
+            icon.attr('name','close') 
         }else{
             navList.removeAttr('style')
+            icon.attr('name','menu')
         }
     })
+
 });
